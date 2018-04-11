32c30436f914c8507479230eea1704007627d58e
Alexander Kravchenko <akravchenko188@gmail.com>
2018-04-11 22:26:08 +0300

    reorg build scripts

--- test_nasa/compare.bat
+++ test_nasa/compare.bat
@@ -1,4 +1,4 @@
-..\..\test_assets\tools\ffmpeg -i %1 -i %2 -filter_complex "blend=all_mode=difference" -c:v libx264 -crf 18 -c:a copy %1.compare.mkv
+..\..\test_assets\tools\ffmpeg -y -i %1 -i %2 -filter_complex "blend=all_mode=difference" -c:v libx264 -crf 18 -c:a copy %1.compare.mkv
 
 
 
