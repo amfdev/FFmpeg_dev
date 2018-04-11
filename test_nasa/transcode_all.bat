ec3b804978a61c0d1b3a9b7f3db6cc713bb1f132
Alexander Kravchenko <akravchenko188@gmail.com>
2018-04-11 22:48:50 +0300

    reorg build scripts

---
+++ test_nasa/transcode_all.bat
@@ -0,0 +1,15 @@
+call transcode_d3d11_host.bat matrixbench_mpeg2.mpg
+call transcode_d3d11_hw.bat matrixbench_mpeg2.mpg
+call transcode_dxva2_host.bat matrixbench_mpeg2.mpg
+call transcode_dxva2_hw.bat matrixbench_mpeg2.mpg
+
+call transcode_d3d11_host.bat nasa_720p.264
+call transcode_d3d11_hw.bat nasa_720p.264
+call transcode_dxva2_host.bat nasa_720p.264
+call transcode_dxva2_hw.bat nasa_720p.264
+
+call transcode_d3d11_host.bat video.mp4
+call transcode_d3d11_hw.bat video.mp4
+call transcode_dxva2_host.bat video.mp4
+call transcode_dxva2_hw.bat video.mp4
+
