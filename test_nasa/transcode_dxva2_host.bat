a1f6c8c776fda619fec1163db2e03709748080fe
Alexander Kravchenko <akravchenko188@gmail.com>
2018-04-11 23:46:38 +0300

    reorg build scripts

--- test_nasa/transcode_dxva2_host.bat
+++ test_nasa/transcode_dxva2_host.bat
@@ -1,4 +1,4 @@
 ..\_build-mingw_gcc_x64-debug\ffmpeg.exe -y -hwaccel dxva2 -i %1 -an -c:v h264_amf %1.dxva2_host.mkv
-.\compare.bat %1 %1.dx11_host.mkv
+.\compare.bat %1 %1.dxva2_host.mkv
 
 
--- test_nasa/transcode_dxva2_hw.bat
+++ test_nasa/transcode_dxva2_hw.bat
@@ -1,3 +1,3 @@
-..\_build-mingw_gcc_x64-debug\ffmpeg.exe -y -hwaccel dxva2 -hwaccel_output_format dxva2_vld -i %1 -an -c:v h264_amf %1.dxva2_hw.mkv
-.\compare.bat %1 %1.dx11_hw.mkv
+..\_build-mingw_gcc_x64-debug\ffmpeg.exe -y -hwaccel dxva2 -hwaccel_output_format dxva2_vld  -i %1 -an -c:v h264_amf %1.dxva2_hw.mkv
+.\compare.bat %1 %1.dxva2_hw.mkv
 
