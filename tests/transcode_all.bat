77b95585e79b95d59b37883d546d60f347b88c28
Alexander Kravchenko <akravchenko188@gmail.com>
2018-04-13 13:10:59 +0300

    lavc/amfenc: DXVA2 textures support implementation by AMF encoder

--- tests/transcode_all.bat
+++ tests/transcode_all.bat
@@ -8,8 +8,8 @@ call transcode_d3d11_hw.bat nasa_720p.264
 call transcode_dxva2_host.bat nasa_720p.264
 call transcode_dxva2_hw.bat nasa_720p.264
 
-call transcode_d3d11_host.bat video.mp4
-call transcode_d3d11_hw.bat video.mp4
-call transcode_dxva2_host.bat video.mp4
-call transcode_dxva2_hw.bat video.mp4
+rem call transcode_d3d11_host.bat video.mp4
+rem call transcode_d3d11_hw.bat video.mp4
+rem call transcode_dxva2_host.bat video.mp4
+rem call transcode_dxva2_hw.bat video.mp4
 
--- tests/transcode_dxva2_host.bat
+++ tests/transcode_dxva2_host.bat
@@ -1,4 +1,4 @@
-..\_build-mingw_gcc_x64-debug\ffmpeg.exe -y -hwaccel dxva2 -i %1 -an -c:v h264_amf %1.dxva2_host.mkv
+..\_build-mingw_gcc_x64-debug\ffmpeg.exe -hide_banner -y -hwaccel dxva2 -i %1 -an -c:v h264_amf %1.dxva2_host.mkv
 .\compare.bat %1 %1.dxva2_host.mkv
 
 
